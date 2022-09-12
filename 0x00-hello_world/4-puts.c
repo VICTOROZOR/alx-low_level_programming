b0ff3fcc30ed93189511f7297468ae1e91a0a53c
VICTOROZOR <victorjoel0061@gmail.com>
2022-09-12 09:21:47 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
-include <stdio.h>
+#include <stdio.h>
+/** 
+ * Description: main - Prints out a string
+ * Return: 0 if success.
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0)
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
