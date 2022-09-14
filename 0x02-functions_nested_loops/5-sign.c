d62c2cb398c0be319f7c478a3f13c0989591b338
VICTOROZOR <victorjoel0061@gmail.com>
2022-09-13 17:55:29 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - function to check sign
+ *@n: for argument
+ *Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar('-')
+	return (-1);
+	else
+	{
+	-putchar('0');
+	return (0);
+	}
+}
